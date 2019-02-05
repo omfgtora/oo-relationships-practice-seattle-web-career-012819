ed40ddc68255e810008dbae6ccd31b74c4df853e
omfgtora <spaminol@gmail.com>
2019-02-05 10:22:42 -0800

    Completed Lyft

--- tools/console.rb
+++ tools/console.rb
@@ -5,7 +5,7 @@ def reload
 end
 
 #
-# airbnb Tests
+# Airbnb Tests
 #
 
 # Listings
@@ -44,12 +44,12 @@ for each in all_listings
 end
 puts "\n"
 most_popular = Listing.most_popular
-if most_popular.length > 1
-  puts "The most popular listings have #{most_popular[0].trip_count} trips total:"
-  most_popular.each { |each| puts "    #{each.name}" }
-else
-  puts "Most Popular Listing is the #{most_popular.name} with #{most_popular.trip_count} trips."
-end
+# if most_popular.length > 1
+#   puts "The most popular listings have #{most_popular[0].trip_count} trips total:"
+#   most_popular.each { |each| puts "    #{each.name}" }
+# else
+#   puts "Most Popular Listing is the #{most_popular.name} with #{most_popular.trip_count} trips."
+# end
 Listing.find_all_by_city("Lost Angeles")
 Guest.all
 Guest.pro_traveler
@@ -66,5 +66,58 @@ bill_murray = Actor.new("Bill Murray")
 andie_macdowell = Actor.new("Andie MacDowell")
 groundhog_day = Movie.new("Groundhog Day")
 groundhog_day.add_character("Phil Connors", "Bill Murray")
+
+#
+# Lyft Tests
+#
+
+# Passengers
+skywalker = Passenger.new("Luke Skywalker")
+solo = Passenger.new("Han Solo")
+leia = Passenger.new("Leia Solo")
+rey = Passenger.new("Rey")
+
+# Driver
+chewie = Driver.new("Chewbacca")
+c3po = Driver.new("C3PO")
+r2d2 = Driver.new("R2D2")
+
+# Rides (distance (float), passenger, driver)
+Ride.new(10.0, skywalker, chewie)
+Ride.new(20.0, leia, c3po)
+Ride.new(15.0, solo, chewie)
+Ride.new(5.0, skywalker, r2d2)
+
+
+puts "\n\nPassengers:"
+Passenger.all.each {|passenger|
+  puts "  " + passenger.name
+}
+puts "\nDrivers:"
+Driver.all.each {|driver|
+  puts "  " + driver.name
+}
+
+# Ride distance
+puts "\n Rides distance:"
+puts "  Skywalker total: " + skywalker.total_distance.to_s
+puts "  Leia total: " + leia.total_distance.to_s
+puts "  Han total: " + solo.total_distance.to_s
+puts "  Rey total: " + rey.total_distance.to_s
+
+puts "\n Average Distance: " + Ride.average_distance.to_s
+
+puts "\n Mileage cap 5: "
+Driver.mileage_cap(5).each {|driver|
+  puts "  " + driver.name
+}
+puts "\n Mileage cap 10: "
+Driver.mileage_cap(10).each {|driver|
+  puts "  " + driver.name
+}
+puts "\n Mileage cap 15: "
+Driver.mileage_cap(15).each {|driver|
+  puts "  " + driver.name
+}
 
 # Pry.start
